2502a4cbb4829ecf2789faf9db016baaef5d8848
Vishu <brs@Vishus-MacBook-Air.local>
2020-03-22 00:06:19 +0530

    Updates

---
+++ precache-manifest.da7f0b2efaf7d3af134c838fd6ffa7db.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860d612f79a26a9d99a8e286c204b5fa",
+    "url": "/IntugineDashboard/index.html"
+  },
+  {
+    "revision": "3a61aca9ff13dd1d7f66",
+    "url": "/IntugineDashboard/static/css/main.6cc36cc2.chunk.css"
+  },
+  {
+    "revision": "d1a4816468ede1544ee3",
+    "url": "/IntugineDashboard/static/js/2.8e8b7fce.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/IntugineDashboard/static/js/2.8e8b7fce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a61aca9ff13dd1d7f66",
+    "url": "/IntugineDashboard/static/js/main.7b7c9693.chunk.js"
+  },
+  {
+    "revision": "d2bd9516a0deddeba663",
+    "url": "/IntugineDashboard/static/js/runtime-main.23e742cf.js"
+  },
+  {
+    "revision": "85f9fdcc34b168fd80fa48babd0e1b99",
+    "url": "/IntugineDashboard/static/media/destination.85f9fdcc.svg"
+  },
+  {
+    "revision": "877653aeb01976fd7edf924c661eacc2",
+    "url": "/IntugineDashboard/static/media/logo.877653ae.svg"
+  },
+  {
+    "revision": "52b703a8be7195bf860d5e89cdc1ec6e",
+    "url": "/IntugineDashboard/static/media/profile.52b703a8.svg"
+  },
+  {
+    "revision": "1f5bfd3e805919a486ffb8e1ab44785a",
+    "url": "/IntugineDashboard/static/media/warehouse.1f5bfd3e.svg"
+  }
+]);
